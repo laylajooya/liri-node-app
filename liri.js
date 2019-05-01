e0988339967bfdfc74daea8dcba489c34aafdb1f
Layla Jooya <laylajooya@gmail.com>
2019-05-01 18:06:50 -0500

    added initial files and apis

--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
+// To read and set any environment variables with dotenv
 require("dotenv").config();
 
+// Import keys.js and store as variable
 var keys = require("./keys.js");
 
 // SPOTIFY API and NODE
@@ -11,3 +13,6 @@ var omdb = (keys.omdb);
 
 // BANDSINTOWN API
 var bandsintown = (keys.bandsintown);
+
+// https://github.com/kat3ng/liri-node-app/blob/master/liri.js
+// https://github.com/the-Coding-Boot-Camp-at-UT/UTAUS201902FSF5/blob/master/10-nodejs/02-Homework/Instructions/homework_instructions.md
