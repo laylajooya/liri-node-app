caff250c6ccc7ad90cebcc34281c91bec94fdcdc
Layla Jooya <laylajooya@gmail.com>
2019-05-01 10:35:53 -0500

    added API keys for omdb and bandsintown

--- liri.js
+++ liri.js
@@ -2,4 +2,12 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+// SPOTIFY API and NODE
+var SPOTIFY = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+
+// OMDB API
+var omdb = (keys.omdb);
+
+// BANDSINTOWN API
+var bandsintown = (keys.bandsintown);
